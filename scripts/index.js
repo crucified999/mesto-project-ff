0e2dc3aca6309d6a735984bd75a19dd10f55e8ce
crucified <khalinarseny@yandex.ru>
2025-01-19 22:59:44 +0300

    mesto-project v1

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,37 @@
 
 // @todo: Функция создания карточки
 
+function createCard(card, deleteCard) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardContent = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const deleteCardButton = cardContent.querySelector('.card__delete-button');
+  
+    cardContent.querySelector('.card__image').src = card.link;
+    cardContent.querySelector('.card__image').alt = card.name;
+    cardContent.querySelector('.card__title').textContent = card.name;
+  
+    deleteCardButton.addEventListener('click', e => {
+        deleteCard(e.target.parentElement);
+    });
+
+    return cardContent;
+  }
+
 // @todo: Функция удаления карточки
 
+function deleteCard(card) {
+
+    card.remove();
+
+}
+
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(el => {
+    
+    const card = createCard(el, deleteCard);
+
+    document.querySelector('.places__list').append(card);
+    
+})
