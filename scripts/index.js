fae38d97ff78298f00836159b3e1a29d3ae80d07
crucified <khalinarseny@yandex.ru>
2025-01-25 14:30:50 +0300

    first review fixes

--- scripts/index.js
+++ scripts/index.js
@@ -5,36 +5,35 @@
 // @todo: Функция создания карточки
 
 function createCard(card, deleteCard) {
-    const cardTemplate = document.querySelector('#card-template').content;
-    const cardContent = cardTemplate.querySelector('.places__item').cloneNode(true);
-    const deleteCardButton = cardContent.querySelector('.card__delete-button');
-  
-    cardContent.querySelector('.card__image').src = card.link;
-    cardContent.querySelector('.card__image').alt = card.name;
-    cardContent.querySelector('.card__title').textContent = card.name;
-  
-    deleteCardButton.addEventListener('click', e => {
-        deleteCard(e.target.parentElement);
-    });
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardContent = cardTemplate
+    .querySelector(".places__item")
+    .cloneNode(true);
+  const deleteCardButton = cardContent.querySelector(".card__delete-button");
+  const cardImage = cardContent.querySelector(".card__image");
 
-    return cardContent;
-  }
+  cardImage.src = card.link;
+  cardImage.alt = card.name;
+  cardContent.querySelector(".card__title").textContent = card.name;
+
+  deleteCardButton.addEventListener("click", () => {
+    deleteCard(cardContent);
+  });
+
+  return cardContent;
+}
 
 // @todo: Функция удаления карточки
 
 function deleteCard(card) {
-
-    card.remove();
-
+  card.remove();
 }
-
 
 // @todo: Вывести карточки на страницу
 
-initialCards.forEach(el => {
-    
-    const card = createCard(el, deleteCard);
+const placesList = document.querySelector(".places__list");
 
-    document.querySelector('.places__list').append(card);
-    
-})
+initialCards.forEach((el) => {
+  const card = createCard(el, deleteCard);
+  placesList.append(card);
+});
